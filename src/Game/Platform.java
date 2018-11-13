3838b042b12cd585fd5c93e30c41ff985d4abb07
agudi24 <30880671+agudi24@users.noreply.github.com>
2018-11-13 07:23:11 -0800

    Update Platform.java

--- src/Game/Platform.java
+++ src/Game/Platform.java
@@ -9,7 +9,10 @@ public class Platform{
 	private int y;
 	private int width;
 	private int height;
-	
+	static int plat1 = 475;
+	static int plat2 = 475;
+	static int plat3 = 475;
+	static int plat4 = 475;
 	private Rectangle cBox = new Rectangle();
 	
 	public Platform(int x, int y, int w, int h){
@@ -17,22 +20,29 @@ public class Platform{
 		this.y = y;
 		this.width = w;
 		this.height = h;
-		
 		cBox.setBounds(x, y, width, height);
 	}
 	
+	
 	public void update(){
 		cBox.setBounds(x, y, width, height);
-		Level2FinalGame.plat1 = Level2FinalGame.plat1 + 2;
-		Level2FinalGame.plat2 = Level2FinalGame.plat2 + 4;
-		Level2FinalGame.plat3 = Level2FinalGame.plat3 + 6;
-		Level2FinalGame.plat4 = Level2FinalGame.plat4 + 8;
-		
+		/*plat1 = plat1 - 2;
+		plat2 = plat2 - 4;
+		plat3 = plat3 - 6;
+		plat4 = plat4 - 8;*/
+		//System.out.println("Moving");
 	}
 	
 	public void draw(Graphics g){
 		g.setColor(Color.GREEN);
 		g.fillRect(x, y, width, height);
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 525, 350, 75);
+		g.fillRect(1600, 525, 350, 75);
+		plat1 = plat1 - 2;
+		plat2 = plat2 - 4;
+		plat3 = plat3 - 6;
+		plat4 = plat4 - 8;
 		
 	}
 	
