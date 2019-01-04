6b9602fed24b069e557f45e545d257b2aeb2c787
agudi24 <30880671+agudi24@users.noreply.github.com>
2019-01-03 22:44:18 -0800

    Update Background.java

--- src/Game/Background.java
+++ src/Game/Background.java
@@ -3,7 +3,7 @@ package Game;
 import java.awt.Graphics;
 
 public class Background {
-	public void drawBackground(Graphics g) {
+	public static void drawBackground(Graphics g) {
 		g.drawImage(FinalGamePanel.backgroundImg, 0, 0, 1950, 600, null);
 	}
 }
